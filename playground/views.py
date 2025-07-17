300991f31ca0c8ab91f092a4cc192aaafa4db2e5
unknown <mahrishisinghrathore@gmail.com>
2025-07-17 23:19:10 +0530

    Django manager and orm learning

--- playground/views.py
+++ playground/views.py
@@ -1,6 +1,7 @@
 # views are more like request handler in django
 from django.shortcuts import render
 from store . models import Product
+from django.db.models import Q
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from . import templates
@@ -24,9 +25,16 @@ def say_hello(request):
     #     product = Product.objects.get(pk=-0)
     # except ObjectDoesNotExist:
     #     pass
-    product=Product.objects.filter(pk=0).first() # here it will not throw any exception it will give None value
+    # product=Product.objects.filter(pk=0).first() # here it will not throw any exception it will give None value
+    # exsist=Product.objects.filter(pk=0).exsist()# here it will return boolean value
+    # queryset=Product.objects.filter(unit_price=200) # here we cannot use directly the operator lik egreather then or lesset then or equall to
+    # queryset = Product.objects.filter(unit_price__gt=200)# now here we are using the keyword agrument  (Keyword and agrument)
+    # queryset = Product.objects.filter(unit_price__gt=200)# now here we are using the keyword agrument  (Keyword and agrument)
+    # for i in queryset:
+    #     print(i.title)
+    # queryset = Product.objects.filter(collection_id__gt=20,unit_price__gt=200)
 
+    queryset=Product.objects.filter(Q(collection_id__gt=20) | Q(unit_price__gt=200)) #use for the or operator
 
-
-    return render(request,'hello.html',{'name':'rishi'})
+    return render(request,'hello.html',{'name':'rishi','products':list(queryset)})
 
